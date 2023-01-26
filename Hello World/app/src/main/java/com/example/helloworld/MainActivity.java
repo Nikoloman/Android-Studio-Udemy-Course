6a8f073cc09befec1c6f5c11d90b2b21130ef64d
Nikoloman <hectorvel52@gmail.com>
2023-01-26 11:29:38 -0600

    A simple app where logs something by pressing the button, ignore the name lol

---
+++ Hello World/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view){
+        Log.i("Info", "Button has been pressed");
+    }
+}
