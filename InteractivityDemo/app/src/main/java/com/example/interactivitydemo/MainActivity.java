e5b3be9c78c5a3737b20183e73cdd6c0431c4dbf
Nikoloman <hectorvel52@gmail.com>
2023-02-16 17:25:32 -0600

    Smth happened, dunno what, but the app works fine lmao

---
+++ InteractivityDemo/app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.interactivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void clickFunction (View view){
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("Info", "Button pressed");
+        Log.i("Name", nameEditText.getText().toString());
+    }
+}
